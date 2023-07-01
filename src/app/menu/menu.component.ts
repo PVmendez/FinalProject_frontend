1753974636fc4eefe14fc7913522f7d6200b5150
PVmendez <valentino.mendez.rey@gmail.com>
2023-06-30 21:50:16 -0300

    feature: user profile front

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,12 +4,8 @@ import { AuthenticationService } from '../services/authentication.service';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent {
-  constructor(private authService: AuthenticationService) {}
 
-  public logOutUser(){
-    this.authService.logout();
-  }
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,19 +1,36 @@
 import { Component } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css']
+  styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
-  user = {};
+  user = {
+    id: '1',
+    username: 'Username',
+    email: 'prueba',
+    full_name: 'Full Name',
+    photo:
+      'https://upload.wikimedia.org/wikipedia/commons/5/5a/John_Doe%2C_born_John_Nommensen_Duchac.jpg',
+  };
 
-  constructor(private profileService: ProfileService) {}
+  selectedSegment: string = 'segment1';
 
-  ngOnInit() {
-    this.profileService.getUser("").then((user) => {
-      this.user = user
-    });
+  selectSegment(segment: string) {
+    this.selectedSegment = segment;
+  }
+
+  constructor(private authService: AuthenticationService, private profileService: ProfileService) {}
+
+  async ngOnInit() {
+    //const user = await this.profileService.getUser('');
+    //this.user = user;
+  }
+
+  public logOutUser(){
+    this.authService.logout();
   }
 }
