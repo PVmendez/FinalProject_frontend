49a3324e70c4967d669df3a41a834ae5144c4ff6
Belen <maria.peirano@correo.ucu.edu.uy>
2023-07-01 18:59:39 -0300

    Fixing comments

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-  ngOnInit() { }
+export class LoginComponent{
 }
