ef665e529505d6a50adcc009464e647565245604
Belen <maria.peirano@correo.ucu.edu.uy>
2023-07-01 23:48:12 -0300

    user profile logic and adapt to ionic

---
+++ src/app/interfaces/notification.ts
@@ -0,0 +1,7 @@
+import { User } from "./user";
+
+export interface Notifications {
+    user: User,
+    description: string,
+    date: string
+}
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ProfileService } from 'src/app/services/profile.service';
 import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../interfaces/user';
+import { Notifications } from '../interfaces/notification';
 
 @Component({
   selector: 'app-user-profile',
@@ -8,7 +10,7 @@ import { AuthenticationService } from '../services/authentication.service';
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
-  user = {
+  user1: User = {
     id: '1',
     username: 'Username',
     email: 'prueba',
@@ -16,6 +18,19 @@ export class UserProfileComponent {
     photo:
       'https://upload.wikimedia.org/wikipedia/commons/5/5a/John_Doe%2C_born_John_Nommensen_Duchac.jpg',
   };
+
+  news: Notifications[] = [
+    {
+      user: this.user1,
+      description: "gagagagagaga",
+      date: '20-07-23'
+    },
+    {
+      user: this.user1,
+      description: "dfgfdfdgsdfgdsfgd",
+      date: '20-07-23'
+    }
+  ];
 
   selectedSegment: string = 'segment1';
 
@@ -27,7 +42,7 @@ export class UserProfileComponent {
 
   async ngOnInit() {
     const user = await this.profileService.getUser();
-    this.user = user;
+    this.user1 = user;
   }
 
   public logOutUser(){
