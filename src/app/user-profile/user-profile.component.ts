314ce62aec62056b993800ff8e41e1704c0aad52
Belen <maria.peirano@correo.ucu.edu.uy>
2023-07-02 02:37:25 -0300

    endpoint user and user interface

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from '../services/authentication.service';
 import { Notifications } from '../interfaces/notification';
 import { DashboardService } from '../services/dashboard.service';
 import { Project } from '../interfaces/project';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-user-profile',
@@ -11,17 +12,13 @@ import { Project } from '../interfaces/project';
   styleUrls: ['./user-profile.component.css'],
 })
 export class UserProfileComponent {
-  user1 = {
-    id: '1',
-    username: 'Username',
-    email: 'prueba',
-    full_name: 'Full Name',
-    photo:
-      'https://upload.wikimedia.org/wikipedia/commons/5/5a/John_Doe%2C_born_John_Nommensen_Duchac.jpg',
+  user: User = {
+    id: '',
+    username: '',
+    email: '',
+    full_name: '',
+    photo: ''
   };
-
-  // notifications = [{"project_name":"ShopApp","risk_level":"High","date":"2023-07-01","levels":{"high":5,"medium":5}},{"project_name":"SocialApp","risk_level":"High","date":"2023-07-01","levels":{"high":15,"medium":1}},{"project_name":"Checkout","risk_level":"Medium","date":"2023-06-29","levels":{"high":2,"medium":4}},{"project_name":"UserBlogger","risk_level":"Medium","date":"2023-06-29","levels":{"high":3,"medium":7}},{"project_name":"EcommerceApp","risk_level":"Low","date":"2023-07-03","levels":{"high":2,"medium":6}},{"project_name":"MobileCommerce","risk_level":"Low","date":"2023-07-02","levels":{"high":1,"medium":0}}];
-
   projectsArray: Project[] = [];
   notifications: Project[] = [];
 
@@ -29,9 +26,8 @@ export class UserProfileComponent {
 
   async ngOnInit() {
     await this.getDatos();
-    console.log(this.notifications);
     const user = await this.profileService.getUser();
-    this.user1 = user;
+    this.user = user;
   }
 
   async getDatos() {
