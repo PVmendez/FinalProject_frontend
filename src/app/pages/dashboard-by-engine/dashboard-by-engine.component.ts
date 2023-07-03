01ed0c162b4b6715afbcc306f5e8f575fffd07ee
Joelit0 <joel.alayon@moove-it.com>
2023-07-02 22:07:22 -0300

    Fixes

--- src/app/pages/dashboard-by-engine/dashboard-by-engine.component.ts
+++ src/app/pages/dashboard-by-engine/dashboard-by-engine.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
 import { Engine } from 'src/app/interfaces/engine';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { ActivatedRoute } from '@angular/router';
@@ -8,7 +8,7 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './dashboard-by-engine.component.html',
   styleUrls: ['./dashboard-by-engine.component.css']
 })
-export class DashboardByEngineComponent implements OnInit {
+export class DashboardByEngineComponent implements OnInit, OnChanges {
   engines: Engine = {
     SAST: 0,
     SCA: 0,
@@ -16,13 +16,23 @@ export class DashboardByEngineComponent implements OnInit {
   };
 
   date: string = '';
+  dataExists: boolean = true;
 
-  constructor(private dashboardService: DashboardService, private route: ActivatedRoute) { }
+  constructor(private dashboardService: DashboardService, private route: ActivatedRoute) {}
 
   async ngOnInit() {
     this.route.queryParams.subscribe(async params => {
       this.date = params['filterByDate'];
       this.engines = await this.dashboardService.getEngines(params['filterByDate'])
+  
+      if (this.engines.SAST == 0 && this.engines.IaC == 0 && this.engines.SCA == 0 && !!this.date) {
+        this.dataExists = false;
+      } else {
+        this.dataExists = true;
+      };
     });
+  }
+
+  ngOnChanges() {
   }
 }
