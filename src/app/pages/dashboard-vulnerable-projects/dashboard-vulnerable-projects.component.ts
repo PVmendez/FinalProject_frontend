e9ea684c18f83e92add449bc9bca68d7ba45b76c
Joelit0 <joel.alayon@moove-it.com>
2023-07-02 22:07:22 -0300

    Fixes

--- src/app/pages/dashboard-vulnerable-projects/dashboard-vulnerable-projects.component.ts
+++ src/app/pages/dashboard-vulnerable-projects/dashboard-vulnerable-projects.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { Project } from 'src/app/interfaces/project';
 import { DashboardService } from 'src/app/services/dashboard.service';
 import { getRiskColor } from 'src/app/helpers/riskColor';
 import { ActivatedRoute } from '@angular/router';
-import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-vulnerable-projects',
@@ -11,20 +10,25 @@ import { filter } from 'rxjs';
   styleUrls: ['./dashboard-vulnerable-projects.component.css']
 })
 
-export class DashboardVulnerableProjectsComponent implements OnInit{
+export class DashboardVulnerableProjectsComponent implements OnInit {
   projectsArray: Project[] = [];
   date: string = '';
+  dataExists: boolean = true;
 
-  constructor(private dashboardService: DashboardService, private route: ActivatedRoute) { }
+  constructor(private dashboardService: DashboardService, private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
     this.route.queryParams.subscribe(async params => {
-      this.date = params['filterByDate']
-      
-      const projectsArray = await this.dashboardService.getMostVulnerableProjects(params['filterByDate'])
-      this.projectsArray = projectsArray;
-    });
+      this.date = params['filterByDate'];
+      this.projectsArray = await this.dashboardService.getMostVulnerableProjects(params['filterByDate']);
 
+      if (this.projectsArray.length == 0 && !!this.date) {
+        this.dataExists = false;
+      } else {
+        this.dataExists = true;
+      };
+    });
   }
 
   getColor(risk: string) {
